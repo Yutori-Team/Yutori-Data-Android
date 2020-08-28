2b559eb29195ecf7683e040ed208c8d09641dad5
LeeYuRi1 <dbf51561590@gmail.com>
2020-08-28 23:52:08 +0900

    연습페이지 draw 추가

--- hangul-tensordroid/app/src/main/java/yutori/tf/hangul/practice/PracticeActivity.kt
+++ hangul-tensordroid/app/src/main/java/yutori/tf/hangul/practice/PracticeActivity.kt
@@ -24,7 +24,7 @@ class PracticeActivity : AppCompatActivity() {
     lateinit var networkService: NetworkService
 
     lateinit var tts : TextToSpeech
-    lateinit var text: String
+    lateinit var speakText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +37,7 @@ class PracticeActivity : AppCompatActivity() {
         networkService = ApplicationController.instance.networkService
         setClickListener()
         playToSpeech()
+        initDraw()
         getSentenceResponse()
     }
 
@@ -52,6 +53,7 @@ class PracticeActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
         btn_practice_prev.setOnClickListener {
             val number = SharedPreferenceController.instance?.getPrefIntegerData("number_of_problem")
             if (number != 1) {
@@ -59,6 +61,10 @@ class PracticeActivity : AppCompatActivity() {
                 val intent = Intent(applicationContext, PracticeActivity::class.java)
                 startActivity(intent)
             }
+        }
+
+        btn_practice_clear.setOnClickListener {
+            clear()
         }
 
     }
@@ -75,12 +81,105 @@ class PracticeActivity : AppCompatActivity() {
 
         btn_practice_speak.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+                tts.speak(speakText, TextToSpeech.QUEUE_FLUSH, null, null)
             } else {
-                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+                tts.speak(speakText, TextToSpeech.QUEUE_FLUSH, null)
             }
         }
+    }
 
+    private fun initDraw() {
+        paint_practice_1.setDrawText(tv_practice_drawHere1)
+        paint_practice_2.setDrawText(tv_practice_drawHere2)
+        paint_practice_3.setDrawText(tv_practice_drawHere3)
+        paint_practice_4.setDrawText(tv_practice_drawHere4)
+        paint_practice_5.setDrawText(tv_practice_drawHere5)
+        paint_practice_6.setDrawText(tv_practice_drawHere6)
+        paint_practice_7.setDrawText(tv_practice_drawHere7)
+        paint_practice_8.setDrawText(tv_practice_drawHere8)
+        paint_practice_9.setDrawText(tv_practice_drawHere9)
+        paint_practice_10.setDrawText(tv_practice_drawHere10)
+        paint_practice_11.setDrawText(tv_practice_drawHere11)
+        paint_practice_12.setDrawText(tv_practice_drawHere12)
+        paint_practice_13.setDrawText(tv_practice_drawHere13)
+        paint_practice_14.setDrawText(tv_practice_drawHere14)
+        paint_practice_15.setDrawText(tv_practice_drawHere15)
+        paint_practice_16.setDrawText(tv_practice_drawHere16)
+    }
+
+    private fun clear() {
+        paint_practice_1.reset()
+        paint_practice_2.reset()
+        paint_practice_3.reset()
+        paint_practice_4.reset()
+        paint_practice_5.reset()
+        paint_practice_6.reset()
+        paint_practice_7.reset()
+        paint_practice_8.reset()
+        paint_practice_9.reset()
+        paint_practice_10.reset()
+        paint_practice_11.reset()
+        paint_practice_12.reset()
+        paint_practice_13.reset()
+        paint_practice_14.reset()
+        paint_practice_15.reset()
+        paint_practice_16.reset()
+        paint_practice_1.invalidate()
+        paint_practice_2.invalidate()
+        paint_practice_3.invalidate()
+        paint_practice_4.invalidate()
+        paint_practice_5.invalidate()
+        paint_practice_6.invalidate()
+        paint_practice_7.invalidate()
+        paint_practice_8.invalidate()
+        paint_practice_9.invalidate()
+        paint_practice_10.invalidate()
+        paint_practice_11.invalidate()
+        paint_practice_12.invalidate()
+        paint_practice_13.invalidate()
+        paint_practice_14.invalidate()
+        paint_practice_15.invalidate()
+        paint_practice_16.invalidate()
+    }
+
+    override fun onResume() {
+        paint_practice_1.onResume()
+        paint_practice_2.onResume()
+        paint_practice_3.onResume()
+        paint_practice_4.onResume()
+        paint_practice_5.onResume()
+        paint_practice_6.onResume()
+        paint_practice_7.onResume()
+        paint_practice_8.onResume()
+        paint_practice_9.onResume()
+        paint_practice_10.onResume()
+        paint_practice_11.onResume()
+        paint_practice_12.onResume()
+        paint_practice_13.onResume()
+        paint_practice_14.onResume()
+        paint_practice_15.onResume()
+        paint_practice_16.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        paint_practice_1.onPause()
+        paint_practice_2.onPause()
+        paint_practice_3.onPause()
+        paint_practice_4.onPause()
+        paint_practice_5.onPause()
+        paint_practice_6.onPause()
+        paint_practice_7.onPause()
+        paint_practice_8.onPause()
+        paint_practice_9.onPause()
+        paint_practice_10.onPause()
+        paint_practice_11.onPause()
+        paint_practice_12.onPause()
+        paint_practice_13.onPause()
+        paint_practice_14.onPause()
+        paint_practice_15.onPause()
+        paint_practice_16.onPause()
+        super.onPause()
     }
 
     override fun onDestroy() {
@@ -113,7 +212,7 @@ class PracticeActivity : AppCompatActivity() {
                         200 -> {
                             toast("200")
                             tv_practice_sentence.setText(response.body()?.resSentenceDtoList?.get(number!!.minus(1))?.sentence.toString())
-                            text = response.body()?.resSentenceDtoList?.get(number!!.minus(1))?.sentence.toString()
+                            speakText = response.body()?.resSentenceDtoList?.get(number!!.minus(1))?.sentence.toString()
                         }
                         400 -> {
                             toast("400")
